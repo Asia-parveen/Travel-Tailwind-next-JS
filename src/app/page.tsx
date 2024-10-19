011e9467bd9f5addc231df42e4694e7a8b377754
Asia-parveen <beautyhealthytips@gmail.com>
2024-10-19 14:26:14 -0700

    new commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,45 @@
-import Image from "next/image";
+import React from 'react'
+import NavBar from './components/common/NavBar';
+import Hero from './components/sections/Hero';
+import CatgorySection from './components/sections/CatgorySection';
+import TopSellingSection from './components/sections/TopSellingSection';
+import BookTripSection from './components/sections/BookTripSection';
+import TestimonialSection from './components/sections/TestimonialSection';
+import LogoGroupSection from './components/sections/LogoGroupSection';
+import NewsLetterSection from './components/sections/NewsLetterSection';
+import FooterSection from './components/sections/FooterSection';
+
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <main className="relative poppins  md:px-[9rem]">
+      <NavBar />
+      <div className="px-4 flex flex-col gap-[7.69rem]">
+        <Hero/>
+        <div className="absolute top-0 right-0 -z-10">
+          <img src="/images/pinkshape.png" alt="blob background shape" />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+        <div className="absolute top-0 left-0 -z-10">
+          <img
+            src="/images/Ellipse 8.png"
+            alt="Ellipse background shape"
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        </div>
+        <div className="relative">
+          <CatgorySection/>
+          <div className="absolute top-0 right-0">
+            <img src="/images/plusbg.png" alt="blob background shape" />
+          </div>
+          </div>
+            <TopSellingSection />
+        <BookTripSection />
+        <TestimonialSection />
+        <LogoGroupSection />
+        <NewsLetterSection/>
+        <FooterSection/>
+      
+      </div>
+    </main>
   );
 }
+       
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,27 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        // background: "var(--background)",
+        // foreground: "var(--foreground)",
+        primary: " #DF6951",
+        secondary: "#F1A501",
+       
+        navText: "#212832",
+        lightBlue: " #181E4B",
+        lightGray: "#5E6282",
+        lightGrayAlt: "#686D77",
+        white: "#FFFFFF",
+        title: "#14183E",
+        subTitle: "#1E1D4C",
+        lightBlack: "#080809",
+        lightGrayAlt2: "#84829A",
+        lightPink: "#BA79DF"
+      },
+      borderRadius: {
+        slight: "0.625rem",
+        normal: "0.3125rem",
+        medium: "1.5rem",
+        extraLarge: "8.0625rem",
       },
     },
   },
